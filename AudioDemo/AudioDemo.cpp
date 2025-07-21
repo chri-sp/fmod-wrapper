d688e55b5fdd62c22a37b52a1a3dd49ccbd8512f
Christian Sipione <christiansipione@gmail.com>
2025-07-21 17:09:34 +0200

    Init project

---
+++ AudioDemo/AudioDemo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "AudioWrapper.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ AudioWrapper/AudioWrapper.cpp
@@ -0,0 +1,12 @@
+// AudioWrapper.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "AudioWrapper.h"
+#include "fmod.hpp"
+
+// TODO: This is an example of a library function
+void fnAudioWrapper()
+{
+}
